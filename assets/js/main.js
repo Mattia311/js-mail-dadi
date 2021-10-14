cbb00a0e4b9a188953b72d8bee03aa60f81c6bfb
Mattia311 <mattia.space98@gmail.com>
2021-10-14 17:14:18 +0200

    esercizio completo

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,17 +1,22 @@
 //Chiedi all’utente la sua email, controlla che sia nella lista di chi può accedere, stampa un messaggio appropriato sull’esito del controllo.
 
 const emailAccess = ['mattia@gmail.com', 'giulio@gmail.com', 'alessio@gmail.com', 'paolo@gmail.com', 'simone@gmail.com']
-
+ 
 
 const askemail = prompt("qual'è la tua email?")
-console.log(emailAccess.length);
 
 for (let i = 0; i < emailAccess.length; i++) {
     const element = emailAccess[i];
-    console.log(element);
-    document.querySelector('accesso').innerHTML = ("L'email inserita è presnte nella lista dei giocatori, puoi partecipare");
-    
+    if (emailAccess.includes(askemail)) {
+        //console.log("l'email è presente, puoi iniziare il gioco");
+        document.getElementById('accesso').innerHTML = ("l'email è presente, puoi iniziare il gioco");
+    }
+    else {
+        console.log("l'email non è presente, riprova");
+        document.getElementById('accesso').innerHTML = ("l'email non è presente, riprova");
+    }
 }
+    
 
 
 
@@ -23,16 +28,21 @@ for (let i = 0; i < emailAccess.length; i++) {
 
  const number = Math.floor(Math.random() * 6) + 1;
  console.log('questo è il tuo numero:',number);
+ 
 
  const number1 = Math.floor(Math.random() * 6) + 1;
  console.log('questo è il numero del computer:',number1);
+ 
 
  if(number > number1) {
      console.log('Hai battuto il computer, il tuo punteggio è più alto');
+     
  } 
  else if(number1 > number) {
      console.log('Mi dispiace, ha vinto il computer');
+     
  } 
  else if(number == number1) {
      console.log('Avete pareggiato, il vostro numero è uguale');
+     
  }
